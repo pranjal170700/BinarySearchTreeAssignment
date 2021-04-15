350e76272a1e5a6c4a1e0bfa551dd43e0263fbdd
Pranjal Singh <singhpranjal6973@gmail.com>
2021-04-16 00:07:00 +0530

    created an object in MyMain.java class

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,11 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
         // and perform insert, search, traversal, delete
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
     }
 }
