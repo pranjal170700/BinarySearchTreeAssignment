26d7142e0ea5ee82a5732996bf65ef5861e9e082
Pranjal Singh <singhpranjal6973@gmail.com>
2021-04-15 23:08:44 +0530

    add getter and setter to Node.java class

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -8,4 +8,28 @@ public class Node<E extends Comparable<E>> implements NodeADT<E> {
     private E data;
     private Node<E> left;
     private Node<E> right;
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public Node<E> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<E> left) {
+        this.left = left;
+    }
+
+    public Node<E> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<E> right) {
+        this.right = right;
+    }
 }
