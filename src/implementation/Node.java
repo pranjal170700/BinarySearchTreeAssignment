44e93eab97438e30f02582ba9981232d5277a407
Pranjal Singh <singhpranjal6973@gmail.com>
2021-04-15 23:07:58 +0530

    add fields to Node.java class

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -5,4 +5,7 @@ import myinterface.NodeADT;
 
 public class Node<E extends Comparable<E>> implements NodeADT<E> {
     //complete node class
+    private E data;
+    private Node<E> left;
+    private Node<E> right;
 }
