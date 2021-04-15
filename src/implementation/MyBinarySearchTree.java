047584d58927ffedd92802cc3992d2654ce8ac04
Pranjal Singh <singhpranjal6973@gmail.com>
2021-04-15 23:29:09 +0530

    implemented insert method in MyBinarySearchTree.java class

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -17,13 +17,29 @@ public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearch
         if(isEmpty()){
             root = node;
         }
+        else {
+            Node<E> temp = root;
+            Node<E> parent = null;
+            while(temp != null) {
+                parent = temp;
+                if(data.compareTo(temp.getData()) <= 0){
+                    temp = temp.getLeft();
+                }
+                else {
+                    temp = temp.getRight();
+                }
+            }
+            if(data.compareTo(parent.getData()) <= 0){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+        }
     }
 
     private boolean isEmpty() {
-        if(root == null){
-            return true;
-        }
-        return false;
+        return root == null;
     }
 
     @Override
