5385a499e965013f1e6918db234e6d947f114e44
Pranjal Singh <singhpranjal6973@gmail.com>
2021-04-16 00:09:06 +0530

    inserted numbers in Binary Search Tree

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -7,5 +7,11 @@ public class MyMain {
         //create binary search tree
         // and perform insert, search, traversal, delete
         MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.insert(35);
+        myBinarySearchTree.insert(70);
+        myBinarySearchTree.insert(65);
+        myBinarySearchTree.insert(90);
+        myBinarySearchTree.insert(85);
     }
 }
